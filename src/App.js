d19ea698bf1bc382fb8090155225a0add4664c06
Joseph Anderson <josephtanderson91@gmail.com>
2022-04-15 14:25:41 -0700

    edit/delete mechanics working

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from "react";
 import uniqid from "uniqid";
 import Overview from "./components/Overview";
+import './App.css'
 
 class App extends Component {
   constructor() {
     super();
-
+    this.handleEditChange = this.handleEditChange.bind(this);
+    this.onEditSubmit = this.onEditSubmit.bind(this);
     this.state = {
       task: {
         text: '',
         id: uniqid()
       },
       tasks: [],
+      editedTask: {}
     };
   }
 
@@ -37,22 +40,73 @@ class App extends Component {
   };
   
   removeTask =(e) => {
-    console.log(this.state.tasks)
+    let taskID = e.target.attributes.partnertask.nodeValue;
+    let index = -1;
+    this.state.tasks.map((ele,ind, arr)=> ele.id === taskID ? index = ind: ele);
+    this.setState(state =>({
+      tasks: state.tasks.slice(0,index).concat(state.tasks.slice(index+1))
+    }))
+  };
+  editBtn = (e) => {
+    let taskID = e.target.attributes.partnertask.nodeValue;
+    let index = -1;
+    this.state.tasks.map((ele,ind, arr)=> ele.id === taskID ? index = ind: ele);
+    let editTask = {
+      edit: true,
+      text: this.state.tasks[index].text,
+      id: this.state.tasks[index].id,
+      previousText: this.state.tasks[index].text
+    }
+    this.setState(state => ({
+      editedTask: editTask,
+      tasks: state.tasks.slice(0,index).concat(editTask).concat(state.tasks.slice(index+1))
+    }))
+  }
+
+  handleEditChange = (e) => {
+    this.setState({
+      editedTask: {
+        edit: true,
+        text: e.target.value,
+        id: this.state.editedTask.id,
+        previousText: this.state.editedTask.previousText
+      }
+    })
+  }
+
+  onEditSubmit = (e) => {
+    e.preventDefault();
+    let index = -1;
+    this.state.tasks.map((ele,ind, arr)=> ele.edit === true ? index = ind: ele);
+    let taskEdit = {
+      text: this.state.editedTask.text,
+      id: this.state.editedTask.id
+    }
+    if (this.state.editedTask.text === '') {
+      taskEdit = {
+        text: this.state.editedTask.previousText,
+        id: this.state.editedTask.id
+      }
+    }
+    this.setState(state => ({
+      tasks: state.tasks.slice(0,index).concat(taskEdit).concat(state.tasks.slice(index+1)),
+      editedTask: {}
+    }));
   };
 
   render() {
-    const { task, tasks } = this.state;
+    const { task, tasks, editedTask } = this.state;
 
     return (
-      <div>
-        <form onSubmit={this.onSubmitTask}>
-          <label htmlFor="taskInput">Enter task</label>
-          <input onChange={this.handleChange} value={task.text} type="text" id="taskInput"/>
+      <div className="app-container">
+        <h1>TASK MANAGER</h1>
+        <form className="add-task-form" onSubmit={this.onSubmitTask}>
+          <input placeholder="Enter Task" onChange={this.handleChange} value={task.text} type="text" id="taskInput" autoComplete="off" /> 
           <button type="submit">
             Add Task
           </button>
         </form>
-        <Overview delButton={this.removeTask} tasks={tasks} />
+        <Overview delButton={this.removeTask} editButton={this.editBtn} editValue={editedTask.text} editChange={this.handleEditChange} editSubmit={this.onEditSubmit} tasks={tasks} />
       </div>
     );
   }
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './Overview.css'
 
 const Overview = (props) => {
   const { tasks } = props;
@@ -6,7 +7,27 @@ const Overview = (props) => {
   return (
     <ul>
       {tasks.map((task) => {
-        return <li key={task.id}>{task.text} <button onClick={props.delButton}>DELETE</button></li>;
+        if (task.edit) {
+          return <li className="edit-item" key={task.id}>
+            <form className="edit-form" onSubmit={props.editSubmit}>
+            <input placeholder={task.text} onChange={props.editChange} value={props.editValue} type="text" id={'edit'+task.id} />
+          <button className="edit-submit" type="submit" partnertask={task.id}>
+            E
+          </button>
+        </form>
+            </li>
+        }
+        return <li className="list-item" key={task.id}>
+            {task.text}
+            <span>
+              <button className="del-button" onClick={props.delButton} partnertask={task.id} >
+                D
+              </button>
+              <button className="edit-button" onClick={props.editButton} partnertask={task.id}>
+                E
+              </button>
+            </span>
+          </li>;
       })}
     </ul>
   );
